72da009af7bb83f80440659de24d821facbcc815
dan <danbickford007@yahoo.com>
2016-04-28 15:40:55 -0400

    adding some params and embed into view

--- web/controllers/hello_controller.ex
+++ web/controllers/hello_controller.ex
@@ -2,8 +2,8 @@ defmodule Foobar.HelloController do
 
   use Foobar.Web, :controller
 
-  def world(conn, _params) do
-    render conn, "world.html"
+  def world(conn, %{"name" => name}) do
+    render conn, "world.html", name: name
   end
 
 end
